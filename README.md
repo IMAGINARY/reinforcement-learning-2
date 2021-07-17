bc0cfa4e5659cdd80cc98559d30d505c36074f1c
Eric Londaits <eric.londaits@gmail.com>
2021-07-17 10:34:56 -0300

    Set up the development environment with webpack and basic web dependencies.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # reinforcement-learning-2
 Reinforcement Learning exhibit for the I AM A.I. exhibition (v2)
+
+## Compilation
+
+To install the required dependencies run `npm install` in the root directory.
+
+You can use `npm run build` or `npm run watch` in the root directory to build the client apps.
+
+The `.env` file in the root directory contains settings that are applied at compilation time.
+
+## License
+
+Copyright (c) 2020-2021 IMAGINARY gGmbH
+Licensed under the MIT license (see LICENSE)
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    default: './src/js/main.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'assets'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/html/index.html'),
+      filename: path.resolve(__dirname, 'index.html'),
+      chunks: ['default'],
+      minify: true,
+    }),
+    new CleanWebpackPlugin({
+      // todo: temporary measure. Dev builds should be done without hashes in the filename.
+      cleanOnceBeforeBuildPatterns: ['**/*'],
+    }),
+  ],
+  mode: 'development',
+  // Todo: change the source map settings for production builds
+  devtool: 'source-map',
+};
